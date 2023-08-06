4e96a1b6afde82a2ad18e5173caea43a122146ae
easion <easion@gmail.com>
2023-08-06 18:19:17 +0800

    Initial commit

---
+++ bin/flutter_pack.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_pack/flutter_pack.dart' as executable;
+
+Future<void> main(List<String> arguments) async {
+  await executable.main(arguments);
+}
---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dart pub global deactivate flutter_pack
+# dart pub global deactivate flutterpi_tool
+#flutter pub global activate --source git https://github.com/easion/flutter_pack.git
+
+flutter pub global activate --source path .
+dart pub global list
+
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+flutter_pack help build
